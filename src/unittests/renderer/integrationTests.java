c7b2c5e5e9e226d74c7480d3cf015c856666d660
שיר באבאייב <hhhbdiha@gmail.com>
2024-06-09 01:49:31 +0300

    push for orginization

--- src/unittests/renderer/integrationTests.java
+++ src/unittests/renderer/integrationTests.java
@@ -33,7 +33,7 @@ public class integrationTests {
 		for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
             	//Calculates the intersection points between each ray that passes through a certain pixel and the geometric body
-            	var list=g.findIntersections(camera.build().constructRay(3, 3, i, j));
+            	var list=g.findIntersections(camera.build().constructRay(3, 3, j, i));
             	if(list!=null)
             		if (firstPoint==false) {
             			intersections=new <Point>LinkedList();
